9890b5a4c3e7d7f670d2c298cee0595e2e5a04d5
Nicolucci <115089042+arthurnicolucci1912@users.noreply.github.com>
2025-09-17 20:05:56 -0300

    add:  operadores

---
+++ UDEMY/operadores/go.mod
@@ -0,0 +1,3 @@
+module GO-Udemy/UDEMY/operadores
+
+go 1.25.1
--- UDEMY/operadores/operadores.go
+++ UDEMY/operadores/operadores.go
@@ -24,4 +24,27 @@ func main() {
 	fmt.Printf("Divisão de %d e %d: %d\n", n1, n2, div)
 	fmt.Printf("Resto de %d e %d: %d \n", n1, n2, mod)
 
+	//atribuição
+	n1 += 1 //n1 = n1+1
+	n1 -= 1 //n1 = n1-1
+	n1 *= 1 //n1 = n1*1
+	n1 /= 1 //n1 = n1/1
+
+	//comparação
+	bigger := n1 > n2
+	smaller := n1 < n2
+	equals := n1 == n2
+	diferent := n1 != n2
+	biggerEqual := n1 >= n2
+	smallerEqual := n1 <= n2
+
+	fmt.Printf("Os numeros %d e %d São iguais?: %v \n", n1, n2, equals)
+	fmt.Printf("Os numeros %d e %d São diferentes?: %v \n", n1, n2, diferent)
+	fmt.Printf("Os numeros %d e %d menores?: %v \n", n1, n2, smaller)
+	fmt.Printf("Os numeros %d e %d maiores?: %v \n", n1, n2, bigger)
+	fmt.Printf("Os numeros %d e %d maiores ou igual: %v \n", n1, n2, biggerEqual)
+	fmt.Printf("Os numeros %d e %d menores ou igual: %v \n", n1, n2, smallerEqual)
+
+	fmt.Print("Valor final de n1 apos as alterações: ", n1)
+
 }
